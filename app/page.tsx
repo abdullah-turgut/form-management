ae6aaa9928ad0700af6a0bb327547f80eea774b3
abdullah-turgut <abdullah.turgut.94@gmail.com>
2023-10-19 15:34:46 +0300

    form elements installed

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+'use client';
+
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useState } from 'react';
+
+const formSchema = z.object({
+  title: z.string().min(1, { message: 'Title is required!' }),
+});
+
 export default function Home() {
   return <h1 className="">Hello Form</h1>;
 }
